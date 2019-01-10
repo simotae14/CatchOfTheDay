70239180708a70e1b19323f6b035d82cdefb4d18
simotae14 <startae14@gmail.com>
2019-01-10 23:58:40 +0100

    Add App, Header, Inventory and Order components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <p>
+                Header
+            </p>
+        );
+    }
+}
+
+export default Header;
