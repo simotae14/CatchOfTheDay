3b2ad8a2d167ce9f57626a2b0ce876ea7187a21e
Simone Taeggi <startae14@gmail.com>
2019-01-11 11:41:19 +0100

    Pass Data with Props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
+        console.log(this);
         return (
-            <p>
-                Header
-            </p>
+            <header className="top">
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    Day
+                </h1>
+                <h3 className="tagline">
+                    <span>
+                        { this.props.tagline }
+                    </span>
+                </h3>
+            </header>
         );
     }
 }
