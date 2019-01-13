c78ad559d71cafe31dc0e7c5ec9f52d09029362a
Simone Taeggi <startae14@gmail.com>
2019-01-13 22:34:23 +0100

    Add Routing to our app

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <h2>
+            Not Found!1111!
+        </h2>
+    );
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App />, document.querySelector('#main'));
+// define the Routing
+const Root = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    component={StorePicker}
+                />
+                <Route
+                    exact
+                    path="/store/:storeId"
+                    component={App}
+                />
+                <Route
+                    component={NotFound}
+                />
+            </Switch>
+        </Router>
+    )
+}
+
+render(<Root />, document.querySelector('#main'));
