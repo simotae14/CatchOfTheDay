593c18b49f9c3a89c17688027dea853af1deed29
Simone Taeggi <startae14@gmail.com>
2019-01-13 22:15:45 +0100

    Revert "Add Pagination in the listings page"
    This reverts commit bcb49e92853bf7f3039bb95a9835527794b4289e.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import './css/style.css';
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
-import NotFound from './components/NotFound';
 
-// define the Routing
-const Root = () => {
-    return (
-        <Router>
-            <div>
-                <Route
-                    exact
-                    path="/"
-                    component={StorePicker}
-                />
-                <Route
-                    exact
-                    path="/store/:storeId"
-                    component={App}
-                />
-                <Route
-                    component={NotFound}
-                />
-            </div>
-        </Router>
-    )
-}
-
-render(<Root />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
