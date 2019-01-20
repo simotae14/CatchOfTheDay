0f96ec04c861c68a7071bb817c6813dbc2a1442e
simotae14 <startae14@gmail.com>
2019-01-20 01:05:42 +0100

    Add event onSubmit and binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        console.log(this);
+        // 3.change the page to /store/whatever they entered
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form
+                className="store-selector"
+                onSubmit={this.goToStore}
+            >
                 <h2>Please Enter A Store</h2>
                 <input
                     type="text"
                     required
                     placeholder="Store Name"
                     defaultValue={getFunName()}
+                    ref={this.myInput}
                 />
                 <button type="submit">Visit Store &rarr;</button>
             </form>
