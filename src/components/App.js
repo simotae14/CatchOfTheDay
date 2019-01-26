cbbd3b8409a8be21a2cc6f8886299fe432d7012b
simotae14 <startae14@gmail.com>
2019-01-26 16:49:30 +0100

    Render the Order inside the View

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,10 @@ class App extends Component {
                         )) }
                     </ul>
                 </div>
-                <Order />
+                <Order
+                    fishes={this.state.fishes}
+                    order={this.state.order}
+                />
                 <Inventory
                     addFish={this.addFish}
                     loadSampleFishes={this.loadSampleFishes}
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends Component {
-    render() {
+    renderOrder = (key) => {
+        // retrieve the fish
+        const fish = this.props.fishes[key];
+        // retrieve the number of items per fish in the order
+        const count = this.props.order[key];
+        // check if the fish is available
+        const isAvailable = fish.status === 'available';
+        if (!isAvailable) {
+            return (
+                <li key={key}>
+                    Sorry { fish ? fish.name : 'fish' } is no longer available
+                </li>
+            );
+        }
         return (
-            <p>
-                Order
-            </p>
+            <li key={key}>
+                {count} lbs {fish.name}
+                { formatPrice(count * fish.price) }
+            </li>
+        );
+    }
+    render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            // retrieve the fish
+            const fish = this.props.fishes[key];
+            // retrieve the number of items per fish in the order
+            const count = this.props.order[key];
+            // check if the fish is not deleted and it's still available
+            const isAvailable = fish && fish.status === 'available';
+            if (isAvailable) {
+                return prevTotal + (count * fish.price);
+            }
+            return prevTotal;
+        }, 0);
+        return (
+            <div className="order-wrap">
+                <h2>
+                    Order
+                </h2>
+                <ul className="order">
+                    {orderIds.map(this.renderOrder)}
+                </ul>
+                <div className="total">
+                    Total:
+                    <strong> {formatPrice(total)}</strong>
+                </div>
+            </div>
         );
     }
 }
