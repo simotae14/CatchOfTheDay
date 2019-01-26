ad67b90a77de2971b341e1f8c7559ad2b3bd9012
simotae14 <startae14@gmail.com>
2019-01-26 15:11:04 +0100

    Displaying the Fish State in the View

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends Component {
     state = {
@@ -31,6 +32,9 @@ class App extends Component {
                     <Header
                         tagline="Fresh Seafood Market"
                     />
+                    <ul className="fishes">
+                        { Object.keys(this.state.fishes).map( key => <Fish key={key} details={this.state.fishes[key]} />) }
+                    </ul>
                 </div>
                 <Order />
                 <Inventory
